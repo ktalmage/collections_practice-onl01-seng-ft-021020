ccbda6880c683aa342819562ab79d6b967abe601
Kyle Talmage <ktalmage85@gmail.com>
2020-02-14 15:06:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,7 +36,7 @@ end
 
 def add_s(array)
    
-   array.each_with_index {|name,index| case when index! = 1 ;name << "s" else index end} 
+   array.each_with_index {|name,index| puts (case when index! = 1 ;name << "s" else index end)} 
     
 end
 
